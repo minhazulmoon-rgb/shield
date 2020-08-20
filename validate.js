745139c5deb6099990670a5e51f7a3bbcf76be19
minhazulmoon-rgb <69197671+minhazulmoon-rgb@users.noreply.github.com>
2020-08-21 03:19:24 +0600

    Add files via upload

---
+++ validate.js
@@ -0,0 +1,43 @@
+var email = document.forms['form']['email'];
+var password = document.forms['form']['password'];
+var email_error = document. getElementById('email_error');
+var pass_error = document. getElementById('pass_error');
+email.addEventListener('textInput', email_Verify);
+password.addEventListener('textInput', pass_Verify);
+
+function validated(){
+
+    if(email.value.length< 9){
+    email.style.border = "2 px solid red";
+    email_error.style.display= "block" ;
+    email.focus();
+    return false;
+}
+
+}
+if(password.value.length< 9){
+    password.style.border = "2 px solid red";
+    password_error.style.display= "block" ;
+    password.focus();
+    return false;
+}
+
+}
+function email_Verify(){
+if(email.value.length >= 8){
+    email.style.border = "1px solid red";
+    email_error.style.display= "block";
+   
+    return true ;
+}
+
+}
+function pass_Verify(){
+if password.value.length >= 8){
+    password_style.border = "1px solid red";
+    password_error.style.display= "none";
+   
+    return true ;
+}
+
+}
